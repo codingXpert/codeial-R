02c8b48adaf0c474d1f107da29e21aefd11a44b7
Vivek <vivekraj78965.vr@gmail.com>
2022-08-31 08:31:32 +0530

    Setting up User Schema

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose  = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+
+    password:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    }
+} , {
+    timestamps:true
+});
+
+
+//creating model
+const User = mongoose.model('User' , userSchema);
+
+module.exports = User;
