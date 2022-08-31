f74f17dd3cab74b4dfc95841480bc687b3a29ab4
Vivek <vivekraj78965.vr@gmail.com>
2022-08-31 09:33:06 +0530

    Creating Forms for Sign up and

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -16,3 +16,13 @@ module.exports.signIn = function(req , res){
         title:"Codeial | Sign in"
     });
 }
+
+//get the sign up data
+module.exports.create = function(req , res){
+// TODO later
+}
+
+//sign in and create session for the user
+module.exports.createSession = function(req , res){
+    // TODO later
+}
