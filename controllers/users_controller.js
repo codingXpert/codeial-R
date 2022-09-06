debe13c5ab8fb9e234d5a5143b29921e255883d9
Vivek <vivekraj78965.vr@gmail.com>
2022-09-06 12:15:01 +0530

    Show Details of Signed in User

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,78 +1,87 @@
- const User = require("../models/user");
+const User = require("../models/user");
 
-module.exports.profile = function(req , res){
-    res.end('<h1>User Profile</h1>');
-}
-
-//render the sign Up page 
-module.exports.signUp = function(req , res){
-    return res.render('user_sign_up' , {
-        title:"Codeial | Sign Up"
+module.exports.profile = function (req, res) {
+  if (req.cookies.user_id) {
+    User.findById(req.cookies.user_id, function (err, user) {
+      if (user) {
+        return res.render("user_profile", {
+          title: "User Profile",
+          user: user,
+        });
+      }else{
+        return res.redirect("/users/sign-in");
+      }
     });
+
+  } else {
+    return res.redirect("/users/sign-in");
+  }
 }
 
+//render the sign Up page
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Codeial | Sign Up",
+  });
+};
 
 //render the sign in paage
-module.exports.signIn = function(req , res){
-    console.log('hello')
-    return res.render('user_sign_in' , {
-        title:"Codeial | Sign in"
-    });
-}
+module.exports.signIn = function (req, res) {
+  console.log("hello");
+  return res.render("user_sign_in", {
+    title: "Codeial | Sign in",
+  });
+};
 
 //get the sign up data
-module.exports.create = function(req , res){
-if(req.body.password != req.body.confirm_password){
-    return res.redirect('back');
-}
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
 
-User.findOne({email:req.body.email} , function(err , user){
-    if(err){
-        console.log('error in finding user in signing up');
-        return;
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("error in finding user in signing up");
+      return;
     }
 
-    if(!user){
-        User.create(req.body , function(err , user){
-            if(err){
-                console.log('error in creating user while signing up');
-                return;
-            }
-            return res.redirect('/users/sign-in')
-        })
-    }else{
-        return res.redirect('back')
+    if (!user) {
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log("error in creating user while signing up");
+          return;
+        }
+        return res.redirect("/users/sign-in");
+      });
+    } else {
+      return res.redirect("back");
     }
-})
-
-}
+  });
+};
 
 //sign in and create session for the user
-module.exports.createSession = function(req , res){
-    // steps for authentication
+module.exports.createSession = function (req, res) {
+  // steps for authentication
 
-    // find the user
-    User.findOne({email:req.body.email} , function(err, user){
-        if(err){
-            console.log('error in finding user in signing in')
-        }
+  // find the user
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("error in finding user in signing in");
+    }
 
-        // handel user found 
-        if(user){
-             
-             // handel password which doesn't match
-             if(user.password != req.body.password){
-              return res.redirect('back');
+    // handel user found
+    if (user) {
+      // handel password which doesn't match
+      if (user.password != req.body.password) {
+        return res.redirect("back");
       }
 
-              //handel session creation
-              res.cookie('user_id' , user.id);
-             return res.redirect('/users/profile');
-
-        }else{
-
-             // handel user not found
-              return res.redirect('back');
-        }
-    });
-}
+      //handel session creation
+      res.cookie("user_id", user.id);
+      return res.redirect("/users/profile");
+    } else {
+      // handel user not found
+      return res.redirect("back");
+    }
+  });
+};
